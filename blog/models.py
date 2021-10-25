e8f9a39a5141d88c1089635f2d8e57fdfc7b1668
nashivan <yarik.nashivan@gmail.com>
2021-10-25 12:25:21 +0300

    admin side

--- blog/models.py
+++ blog/models.py
@@ -22,6 +22,9 @@ class Post(models.Model):
     class Meta:
         ordering = ('-created_at', )
 
+    def __str__(self):
+        return self.title
+
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
@@ -29,3 +32,6 @@ class Comment(models.Model):
     email = models.EmailField()
     body = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
